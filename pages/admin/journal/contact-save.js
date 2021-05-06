600788cdb9e0d5c9c8c2073a46e0c9bd72fb77ea
Ahsanmir28hp <ahsanmir28@gmail.com>
2021-05-06 14:58:14 +0500

    fox

--- pages/admin/journal/contact-save.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import React from 'react';
-import { ContactForm } from '@/components/journal/create/components';
-
-const ContactCreate = () => {
-  return <ContactForm />;
-};
-
-export default ContactCreate;
--- pages/admin/user.js
+++ pages/admin/user.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import User from '../../components/admin-dashboard/user';
 
